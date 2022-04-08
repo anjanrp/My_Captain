3ba574b16685689c252e6afa031949ff3ed8196d
anjanrp <103268584+anjanrp@users.noreply.github.com>
2022-04-08 20:49:11 +0530

    update project2.c
    Write a C Program to perform the swapping of 2 numbers without using a temporary variable.

--- project2.c
+++ project2.c
@@ -4,9 +4,9 @@ int main()
     
 int a=99, b=87;      
 printf("Before swap a=%d b=%d",a,b);      
-a=a+b;//a=186 (10+20)    
-b=a-b;//b=99(30-20)    
-a=a-b;//a=87 (30-10)    
+a=a+b;//a=186 (99+87)    
+b=a-b;//b=99(186-87)    
+a=a-b;//a=87 (186-99)    
 printf("\nAfter swap a=%d b=%d",a,b);    
 return 0;  
  
