b67ba3a7d91104f5aafc2c9f4dd8c6f4bfa2abc8
anjanrp <103268584+anjanrp@users.noreply.github.com>
2022-04-15 17:29:11 +0530

    Create Project5_3.c

---
+++ Project5_3.c
@@ -0,0 +1,19 @@
+// I did'nt quite understand the third task under this .
+// It said on the website that a right angle trianngle needs to be printed with '01'
+// But the same program is submitted as Project5_1.c . 
+// Here is my understanding of the third task 
+
+
+#include<stdio.h> 
+int main()
+{ int i, j; 
+for(i=0;i<7;++i)
+{ 
+    for(j=0;j<=i;++j)
+{ 
+    printf("0690");
+     } 
+     printf("\n"); 
+} 
+return 0; 
+}
